15ccd676ab02f8fefb59c302efca34270a5c8c4b
AndreWWWolf <andrew.wolfeus@gmail.com>
2019-09-09 10:05:27 -0700

    shuffled around folder structure for easier git committing

--- amazon_solution/amazon_public_solution_may_2019/db/migrate/20190215221530_change_price_to_float.rb
+++
@@ -1,5 +0,0 @@
-class ChangePriceToFloat < ActiveRecord::Migration[5.2]
-  def change
-    change_column :products, :price, :float
-  end
-end
--- amazon_solution/amazon_public_solution_may_2019/db/migrate/20190226155014_add_hidden_field_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddHiddenFieldToReviews < ActiveRecord::Migration[5.2]
-  def change
-    add_column :reviews, :hidden, :boolean, default: false
-  end
-end
